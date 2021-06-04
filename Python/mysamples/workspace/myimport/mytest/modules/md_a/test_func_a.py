82e487a2ca30be7fef897aee2603a664c39d5762
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2021-06-05 01:50:37 +0900

    mytestパッケージからパス指定してimportするように修正

--- Python/mysamples/workspace/myimport/mytest/modules/md_a/test_func_a.py
+++ Python/mysamples/workspace/myimport/mytest/modules/md_a/test_func_a.py
@@ -4,6 +4,6 @@ import sys
 PYPATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/"
 sys.path.append(PYPATH + "./../") # main・testディレクトリの場所．testディレクトリ下のモジュールからimportもあり得るため．
 import unittest
-from mytest_tools.md_tool_c import func_c # カレントディレクトリからのimportは，sys.path.append()で上のディレクトリを指定してそこからの絶対Pimportできず，カレントディレクトリからの相対importでないといけない…？
+from mytest.mytest_tools.md_tool_c import func_c # カレントディレクトリからのimportは，sys.path.append()で上のディレクトリを指定してそこからの絶対Pimportできず，カレントディレクトリからの相対importでないといけない…？
 
 
