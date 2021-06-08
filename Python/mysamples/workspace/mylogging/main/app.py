2c76d27a032f9f6e1dac806093a92a89add6c61f
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2021-06-09 03:40:08 +0900

    自作のloggingを実装してみた

--- Python/mysamples/workspace/mylogging/main/app.py
+++ Python/mysamples/workspace/mylogging/main/app.py
@@ -3,11 +3,13 @@ import os
 import sys
 PYPATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/"
 sys.path.append(PYPATH + ".")
+import re
 from conf.conf_logging import CONF_LOGGER
 from modules.logger.logger import (
     init_logging,
     get_logger
 )
+from modules.logger.mylogger import MyLogger
 from modules.md_a.a import (
     func_warn, func_error
 )
@@ -25,6 +27,17 @@ def main():
 
     func_warn("hoge", get_logger = get_logger)
     func_error("fuga")
+
+
+
+    mylogger = MyLogger(
+        "INFO",
+        fname = PYPATH + "./../logs/mytest.log")
+    mylogger.debug("MyLogger debug")
+    mylogger.info("MyLogger info")
+    mylogger.error(
+        "MyLogger error",
+        appname = re.split(r"[\\/]", inspect.getfile(inspect.currentframe()))[-1])
 
 
 
---
+++ Python/mysamples/workspace/mylogging/main/modules/logger/mylogger.py
@@ -0,0 +1,78 @@
+import inspect
+import os
+import sys
+PYPATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/"
+from datetime import datetime as dt
+from modules.myutils.mysingleton import Singleton
+
+class MyLogger(Singleton):
+    __MAP_LOGLEVEL = {
+        "DEBUG": 1,
+        "INFO": 2,
+        "WARNING": 3,
+        "ERROR": 4,
+        "CRITICAL": 5
+    }
+    __loglevel = ""
+    __fmt_log = ""
+    __fmt_date = ""
+    __console = True
+    __fname = None
+    __appname = ""
+    def _init_instance(self,
+        loglevel,
+        fmt_log = "{asctime}.{msecs:03d}@{appname}:{levelname}: {message}",
+        fmt_date = "{Y:04d}-{m:02d}-{d:02d} {H:02d}:{M:02d}:{S:02d}",
+        console = True,
+        fname = None,
+        appname = "APPNAME"):
+        if not loglevel in MyLogger.__MAP_LOGLEVEL.keys():
+            raise ValueError(
+                "loglevel should selected from {}".format(MyLogger.__MAP_LOGLEVEL.keys()))
+        MyLogger.__loglevel = loglevel
+        MyLogger.__fmt_log = fmt_log
+        MyLogger.__fmt_date = fmt_date
+        MyLogger.__console = console
+        MyLogger.__fname = fname
+        MyLogger.__appname = appname
+
+        os.makedirs("/".join(MyLogger.__fname.split("/")[:-1]) + "/", exist_ok = True)
+
+    @staticmethod
+    def __output(msg, loglevel, appname = None) -> Exception:
+        if MyLogger.__MAP_LOGLEVEL[loglevel] < MyLogger.__MAP_LOGLEVEL[MyLogger.__loglevel]:
+            return None
+        now = dt.now()
+        log = MyLogger.__fmt_log.format(
+            asctime = MyLogger.__fmt_date.format(
+                Y = now.year, m = now.month, d = now.day,
+                H = now.hour, M = now.minute, S = now.second),
+            msecs = now.microsecond,
+            appname = appname if appname else MyLogger.__appname,
+            levelname = loglevel,
+            message = msg)
+        if MyLogger.__fname is not None:
+            try:
+                with open(MyLogger.__fname, mode = "a", encoding = "utf-8") as f:
+                    f.write(log + "\n")
+            except Exception as e:
+                return e
+        if MyLogger.__console:
+            print(log)
+        return None
+
+    @staticmethod
+    def debug(msg, appname = None):
+        MyLogger.__output(msg, "DEBUG", appname)
+    @staticmethod
+    def info(msg, appname = None):
+        MyLogger.__output(msg, "INFO", appname)    
+    @staticmethod
+    def warn(msg, appname = None):
+        MyLogger.__output(msg, "WARNING", appname)
+    @staticmethod
+    def error(msg, appname = None):
+        MyLogger.__output(msg, "ERROR", appname)
+    @staticmethod
+    def critical(msg, appname = None):
+        MyLogger.__output(msg, "CRITICAL", appname)
