94ce227d0f60931b5b7160ab523f715b85de07fd
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2022-06-16 10:37:45 +0900

    myimportの実装コードを微修正．ルールのメモも記載．

--- Python/mysamples/workspace/myimport/main/app.py
+++ Python/mysamples/workspace/myimport/main/app.py
@@ -1,6 +1,13 @@
 import inspect
 import os
 import sys
-PYPATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/"
-sys.path.append(PYPATH + ".")
+PYPATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+'''
+- importのRootPathをどこにするか
+    - 基本的に本番用コードを配置する"main/"をRootPathにすると良さげ．
+        - importできるPythonコードの範囲に制限をある程度設け，"mytest/"からテストコードを間違って呼び出すことを防げるから．
+    - 本番リリースが無い個人開発とかで楽したいなら"main/"・"mytest/"の上にあるPJディレクトリをRootPathにしても良いかな．
+'''
+ROOTPATH = "{}/."
+sys.path.append(ROOTPATH)
 from modules.md_a.a import funcA
