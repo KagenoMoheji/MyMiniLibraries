c88fed2af7bf1370258bb0c8468399fcf656e151
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2022-06-16 13:12:05 +0900

    例外クラスそのままにコメントを書き換えるサンプル追加

--- Python/mysamples/workspace/myexception/main/app.py
+++ Python/mysamples/workspace/myexception/main/app.py
@@ -6,4 +6,7 @@ sys.path.append(PYPATH + ".")
 from modules.exception.test import TestError
 
 if __name__ == "__main__":
-    raise TestError("テスト例外だよ！")
+    try:
+        raise TestError("テスト例外だよ！")
+    except Exception as e:
+        raise type(e)("コメントを書き換えたテスト例外だよ！ Detail:{}".format(e))
