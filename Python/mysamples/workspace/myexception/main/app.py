8082cb8fd5c2cb7dbbf2f08460afc3e09d2a32c2
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2022-07-05 23:27:43 +0900

    tracebackの使用例を追加

--- Python/mysamples/workspace/myexception/main/app.py
+++ Python/mysamples/workspace/myexception/main/app.py
@@ -3,10 +3,14 @@ import os
 import sys
 PYPATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/"
 sys.path.append(PYPATH + ".")
+
+import traceback
 from modules.exception.test import TestError
 
 if __name__ == "__main__":
     try:
         raise TestError("テスト例外だよ！")
     except Exception as e:
-        raise type(e)("コメントを書き換えたテスト例外だよ！ Detail:{}".format(e))
+        raise type(e)("コメントを書き換えたテスト例外だよ！ Detail:{err}".format(
+            err = traceback.format_exc()
+        ))
